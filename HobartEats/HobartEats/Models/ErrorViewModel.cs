fd44efa3d477d31ac48f9b5e2478bf9ba8f7c378
pumpernickel <lettucestar1990@gmail.com>
2026-02-06 18:02:25 +1100

    init ASP.NET Core MVC project

---
+++ HobartEats/HobartEats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HobartEats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
