98cc366ff1711a4caabbd5894fa823d84088325f
pumpernickel <lettucestar1990@gmail.com>
2026-02-09 22:29:50 +1100

    Configure EF Core + SQLite and create initial database
    - Added EF Core and SQLite packages
    - Created ApplicationDbContext
    - Configured SQLite connection string
    - Registered DbContext in Program.cs
    - Created App_Data directory
    - Added initial EF Core migration and generated SQLite database
    
    Project now starts successfully with EF Core and SQLite configured.

---
+++ HobartEats/HobartEats/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace HobartEats.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Restaurant> Restaurants { get; set; }
+
+
+    }
+}
